fee80ed791af1980e89dc2c0ce509cc75fced76b
vxm52 <vaibhav.s.malla@gmail.com>
2021-07-08 15:55:27 -0400

    Updates to UI

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Login from "./Login";
+
 function App() {
     return (
         <div style={{ fontFamily: 'Avenir' }}>
             <Router>
                 { /* AuthProvider */ }
                     <Switch>
-                        {/* <Route path="/chats" component={Chats} />
-                        <Route path="/" component={Login} /> */}
+                        {/* <Route path="/chats" component={Chats} /> */}
+                        <Route path="/" component={Login} />
                     </Switch>
                 { /* </AuthProvider> */ }
             </Router>
